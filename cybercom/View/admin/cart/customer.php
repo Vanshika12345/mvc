ff32d84553b25b71cd1086b8249cba4dc2bb6f2c
Vanshika <vanshikaagarwal544@gmail.com>
2021-04-09 19:28:44 +0530

    changes updated

--- cybercom/View/admin/cart/customer.php
+++
@@ -1,9 +0,0 @@
-<?php $customers = $this->getCustomers()->getData();?>
-<div class="mt-3">
-	<select class="form-control" onchange="object.resetParam().setForm('#cartForm').setUrl('<?php echo $this->getUrl()->getUrl('selectCustomer','admin_cart') ?>').setMethod('POST').load()" name="customerId">
-		<option>Select Customer</option>
-		<?php foreach ($customers as $key => $value): ?>
-			 <option value="<?= $value->customerId ?>"><?= $value->fname ?></option>
-		<?php endforeach ?>
-	</select>
-</div>
